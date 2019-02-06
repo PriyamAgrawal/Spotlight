9046768c07f247af812245ea812e3fd4c24000a8
Priyam Agrawal <33418629+PriyamAgrawal@users.noreply.github.com>
2019-02-06 11:45:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost="localhost";
 $dbuser="root";
-$dbpass="qwerty123456";
+$dbpass="";
 $dbname="spotlight";
 $connection=mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
 if(mysqli_connect_errno()){
